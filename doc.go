4b69220905c358159b8bc87c33c9d5045a27c379
shibingli@yeah.net <shibingli@yeah.net>
2022-06-25 17:51:09 +0800

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package spacetrack provides a Go SDK based on the space-track.org RESTFul API.
+
+Reference: [https://www.space-track.org/documentation#/api](https://www.space-track.org/documentation#/api)
+*/
+package spacetrack
